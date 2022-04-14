9734b46cf9b7bd34d883c10b9106c0174ca5861f
Jaeyeol Lee <rijgndqw012@gmail.com>
2022-04-15 02:23:35 +0900

    feat(Screenshot): Add VideoPlayer Component

---
+++ pages/screenshot.tsx
@@ -0,0 +1,73 @@
+import type { NextPage } from 'next'
+
+import React, { useState, useEffect } from "react";
+
+import tw from 'twin.macro';
+import styled from '@emotion/styled';
+
+
+const VideoPlayerWrapper = styled.div`
+	${tw`cursor-pointer`}
+`;
+
+type VideoPlayerProps = {
+	src: string;
+}
+
+const VideoPlayer = ({ src }: VideoPlayerProps) => {
+	const onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		const wrapperElement: HTMLDivElement = e.currentTarget;
+		const videoElement: HTMLMediaElement | null = wrapperElement.querySelector('video');
+		if (!videoElement?.duration) return;
+
+		console.log(wrapperElement);
+		console.log(videoElement);
+
+		if (videoElement.paused) {
+			videoElement.play();
+		} else {
+			videoElement.pause();
+		}
+	}
+
+	return (
+		<VideoPlayerWrapper onClick={onClick}>
+			<video src={src} />
+		</VideoPlayerWrapper>
+	)
+}
+
+
+const ScreenshotCaptureLab: NextPage = () => {
+	const [src, setSrc] = useState<string>("");
+	const [duration, setDuration] = useState<number>(0);
+
+	useEffect(() => {
+		if (!!src) {
+			const videoElement: HTMLMediaElement = document.querySelector('video') as HTMLMediaElement
+			videoElement?.play();
+			setDuration(videoElement.duration);
+		}
+	}, [src])
+
+	const onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (!e.target.files) return;
+
+		const file: File = e.target.files[0];
+
+		const videoUrl = URL.createObjectURL(file);
+		setSrc(videoUrl);
+	}
+
+	return (
+		<div>
+			<input type="file" onChange={onUpload} />
+			<VideoPlayer src={src} />
+			<p>
+				재생 시간 : {duration} 초
+			</p>
+		</div>
+	);
+};
+
+export default ScreenshotCaptureLab;
